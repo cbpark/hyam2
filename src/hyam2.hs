43c6f4024e69b017d49f206efb997c79121f1a7e
cbpark <cbpark@gmail.com>
2020-06-29 13:58:56 +0900

    A simple test (mTtrue)

--- src/hyam2.hs
+++ src/hyam2.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           HEP.Data.LHEF              (getLHEFEvent)
+import           HEP.Data.LHEF
 
 import           Codec.Compression.GZip     (decompress)
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -8,6 +8,7 @@ import           Pipes
 import           Pipes.ByteString           (fromLazy)
 import qualified Pipes.Prelude              as P
 
+import           Data.Maybe                 (fromMaybe)
 import           System.Environment         (getArgs)
 
 main :: IO ()
@@ -18,4 +19,27 @@ main = do
     events <- decompress <$> B.readFile infile
     runEffect $ getLHEFEvent fromLazy events
         >-> P.take 3
+        >-> P.map selectP
+        >-> P.map mTtot
         >-> P.print
+
+selectP :: Event -> Maybe ([FourMomentum], TransverseMomentum)
+selectP ev = do
+    let topChild = particlesFrom topQuarks (eventEntry ev)
+    if null topChild
+        then Nothing
+        else do
+            let pVis = momentumSum' . filter (not . isNeutrino) <$> topChild
+                pNu  = sum $ momentumSum' . filter isNeutrino   <$> topChild
+                ptmiss = transverseVector pNu
+            return (pVis, ptmiss)
+  where
+    topQuarks = ParticleType [6]
+    isNeutrino = (`elem` neutrinos) . idOf
+    momentumSum' = momentumSum . fmap fourMomentum
+
+mTtot :: Maybe ([FourMomentum], TransverseMomentum) -> Double
+mTtot = fromMaybe 0 . mTtot'
+  where
+    mTtot' ps = do (pVis, ptmiss) <- ps
+                   return $ transverseMass pVis (promoteTV ptmiss 0)
