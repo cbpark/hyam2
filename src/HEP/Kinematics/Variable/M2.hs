02ef75cef0012927b4a9597cbf1fe18144314f26
cbpark <cbpark@gmail.com>
2020-07-02 16:12:59 +0900

    Rescale all the scalar values

--- src/HEP/Kinematics/Variable/M2.hs
+++ src/HEP/Kinematics/Variable/M2.hs
@@ -17,6 +17,7 @@ data InputKinematics = InputKinematics
                        , _ptmiss :: TransverseMomentum
                          -- | squared mass of the invisible particle
                        , _mInvSq :: !Double
+                       , _scale  :: !Double
                        }
 
 -- | creates the input for A1 + A2 --> a1 B1 + a2 B2 --> a1 b1 C1 + a2 b2 C2.
@@ -29,12 +30,29 @@ mkInput as bs ptmiss mInv
     | length as /= 2 || length bs /= 2 = Nothing
     | otherwise                        = do
           let ps = zipWith (+) as bs
-          return $ InputKinematics { _p1 = head ps
-                                   , _p2 = ps !! 1
-                                   , _q1 = head bs
-                                   , _q2 = bs !! 1
-                                   , _ptmiss = ptmiss
-                                   , _mInvSq = mInv * mInv
+              p1 = head ps
+              p2 = ps !! 1
+
+              getScale v = let ptv = pt v in ptv * ptv
+              m1 = mass p1
+              m2 = mass p2
+              mInvSq = mInv * mInv
+              scale = sqrt $ (getScale p1 + getScale p2 + getScale ptmiss
+                             + m1 * m1 + m2 * m2 + 2 * mInvSq) / 8.0
+
+              p1' = p1 ^/ scale
+              p2' = p2 ^/ scale
+              q1' = head bs ^/ scale
+              q2' = (bs !! 1) ^/ scale
+              ptmiss' = ptmiss ^/ scale
+              mInv' = mInv / scale
+          return $ InputKinematics { _p1 = p1'
+                                   , _p2 = p2'
+                                   , _q1 = q1'
+                                   , _q2 = q2'
+                                   , _ptmiss = ptmiss'
+                                   , _mInvSq = mInv' * mInv'
+                                   , _scale = scale
                                    }
 
 data M2Solution = M2Solution { _M2    :: Double
@@ -53,7 +71,7 @@ m2SQP (Just inp@InputKinematics {..}) = do
         c2 = EqualityConstraint (Scalar c2fD) 1e-6
 
         -- stop = ObjectiveRelativeTolerance 1e-9 :| []
-        stop = ObjectiveRelativeTolerance 1e-9 :| [MaximumEvaluations 5000]
+        stop = ObjectiveAbsoluteTolerance 1e-9 :| [MaximumEvaluations 5000]
         algorithm = SLSQP objfD [] [] [c1, c2]
         problem = LocalProblem 4 stop algorithm
 
@@ -61,13 +79,12 @@ m2SQP (Just inp@InputKinematics {..}) = do
         sol = minimizeLocal problem x0
     case sol of
         Left _                     -> Nothing
-        Right (Solution m2sq ks _) -> do let m2 = sqrt0 m2sq
-                                             [k1x, k1y, k1z, k2z] = toList ks
-                                             var = (k1x, k1y, k1z, k2z)
-                                             Invisibles k1 k2 = mkInvisibles inp var
-                                         return $ M2Solution { _M2 = m2
-                                                             , _k1sol = k1
-                                                             , _k2sol = k2 }
+        Right (Solution m2sq ks _) -> do
+            let m2 = sqrt0 m2sq
+                Invisibles k1 k2 = mkInvisibles inp (vecToVars ks)
+            return $ M2Solution { _M2 = m2 * _scale
+                                , _k1sol = k1 ^* _scale
+                                , _k2sol = k2 ^* _scale }
   where
     sqrt0 x = if x < 0 then 1.0e+10 else sqrt x
 
@@ -81,18 +98,19 @@ mkInvisibles InputKinematics {..} (k1x, k1y, k1z, k2z) = Invisibles k1 k2
   where
     eInv1 = sqrt (k1x * k1x + k1y * k1y + k1z * k1z + _mInvSq)
     k1 = eInv1 `seq` setXYZT k1x k1y k1z eInv1
+    -- k1' = k1 ^/ _scale
     k2x = px _ptmiss - k1x
     k2y = py _ptmiss - k1y
     eInv2 = sqrt (k2x * k2x + k2y * k2y + k2z * k2z + _mInvSq)
     k2 = eInv2 `seq` setXYZT k2x k2y k2z eInv2
+    -- k2' = k2 ^/ _scale
 
 -- | the unknowns are (k1x, k2x, k1z, k2z).
 m2ObjF :: InputKinematics -> Vector Double -> (Double, Vector Double)
 m2ObjF inp@InputKinematics {..} ks =
     if m1sq < m2sq then (m2sq, grad2) else (m1sq, grad1)
   where
-    [k1x, k1y, k1z, k2z] = toList ks
-    vars = (k1x, k1y, k1z, k2z)
+    vars = vecToVars ks
     (m1sq, m2sq, Invisibles k1 k2) = m2ObjF' inp vars
     (grad1, grad2, _) = m2Grad _p1 _p2 k1 k2 _ptmiss vars
 
@@ -129,26 +147,21 @@ m2Grad p1 p2 k1 k2 ptmiss (k1x, k1y, k1z, k2z) = (d1, d2, deltaMsq)
     deltaMsq = invariantMassSq [p1 + k1] - invariantMassSq [p2 + k2]
 
 constraintA :: InputKinematics -> Vector Double -> (Double, Vector Double)
-constraintA inp@InputKinematics {..} ks
-    | length ks0 /= 4 = badOutput
-    | otherwise       =
-      let [k1x, k1y, k1z, k2z] = ks0
-          vars = (k1x, k1y, k1z, k2z)
-          (m1sq, m2sq, Invisibles k1 k2) = m2ObjF' inp vars
-          (grad1, grad2, _) = m2Grad _p1 _p2 k1 k2 _ptmiss vars
-      in (m1sq - m2sq, grad1 - grad2)
-  where ks0 = toList ks
+constraintA inp@InputKinematics {..} ks = (m1sq - m2sq, grad1 - grad2)
+  where
+    vars = vecToVars ks
+    (m1sq, m2sq, Invisibles k1 k2) = m2ObjF' inp vars
+    (grad1, grad2, _) = m2Grad _p1 _p2 k1 k2 _ptmiss vars
 
 constraintB :: InputKinematics -> Vector Double -> (Double, Vector Double)
-constraintB inp@InputKinematics {..} ks
-    | length ks0 /= 4 = badOutput
-    | otherwise       =
-      let [k1x, k1y, k1z, k2z] = ks0
-          vars = (k1x, k1y, k1z, k2z)
-          (_, _, Invisibles k1 k2) = m2ObjF' inp vars
-          (grad1, grad2, deltaMsq) = m2Grad _q1 _q2 k1 k2 _ptmiss vars
-      in (deltaMsq, grad1 - grad2)
-  where ks0 = toList ks
+constraintB inp@InputKinematics {..} ks = (deltaMsq, grad1 - grad2)
+  where
+    vars = vecToVars ks
+    (_, _, Invisibles k1 k2) = m2ObjF' inp vars
+    (grad1, grad2, deltaMsq) = m2Grad _q1 _q2 k1 k2 _ptmiss vars
+
+vecToVars :: Vector Double -> Variables
+vecToVars ks = let [k1x, k1y, k1z, k2z] = toList ks in (k1x, k1y, k1z, k2z)
 
 safeDivisor :: Double -> Double
 safeDivisor x | x >= 0    = max eps x
