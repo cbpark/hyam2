f9fac5e3cd62e0b8f9f952291387be2731fa37f7
cbpark <cbpark@gmail.com>
2020-07-03 21:36:35 +0900

    Use invariant masses instead of their squares

--- src/HEP/Kinematics/Variable/M2.hs
+++ src/HEP/Kinematics/Variable/M2.hs
@@ -8,6 +8,8 @@ import HEP.Kinematics.Vector.LorentzVector (setXYZT)
 import Numeric.LinearAlgebra               (Vector, fromList, toList)
 import Numeric.NLOPT
 
+import Debug.Trace
+
 -- | All the components are rescaled by '_scale'.
 data InputKinematics = InputKinematics
                        { _p1     :: FourMomentum  -- ^ p1 = a1 + b1
@@ -39,11 +41,11 @@ mkInput as bs ptmiss mInv
               m1 = mass p1
               m2 = mass p2
               mInvSq = mInv * mInv
-              scaleSq = (getScale p1 + getScale p2 + getScale ptmiss
-                         + m1 * m1 + m2 * m2 + 2 * mInvSq) / 8.0
+              scaleSq = getScale p1 + getScale p2 + getScale ptmiss
+                        + m1 * m1 + m2 * m2 + 2 * mInvSq
           if scaleSq <= 0  -- why?
               then Nothing
-              else do let scale = sqrt scaleSq
+              else do let scale = trace ("scale = " ++ show (sqrt scaleSq)) sqrt scaleSq
                           p1'     = p1        ^/ scale
                           p2'     = p2        ^/ scale
                           q1'     = head bs   ^/ scale
@@ -73,14 +75,17 @@ m2SQP (Just inp@InputKinematics {..}) =
     let -- objective function with gradient
         objfD = m2ObjF inp
 
+        eps1 = 1e-3 * _scale
         -- constraint function with gradient
         c1fD = constraintA inp
-        c1 = EqualityConstraint (Scalar c1fD) 1e-9
+        c1 = EqualityConstraint (Scalar c1fD) eps1
         c2fD = constraintB inp
-        c2 = EqualityConstraint (Scalar c2fD) 1e-9
+        c2 = EqualityConstraint (Scalar c2fD) eps1
 
+        eps2 = 1e-6 * _scale
+        -- eps2 = 1e-9
         -- stop = ObjectiveRelativeTolerance 1e-9 :| []
-        stop = ObjectiveAbsoluteTolerance 1e-9 :| [MaximumEvaluations 5000]
+        stop = ObjectiveAbsoluteTolerance eps2 :| [ObjectiveRelativeTolerance eps2]
         algorithm = SLSQP objfD [] [] [c1, c2]
         problem = LocalProblem 4 stop algorithm
 
@@ -93,14 +98,11 @@ getM2Solution :: InputKinematics -> Either Result Solution -> Maybe M2Solution
 getM2Solution inp@InputKinematics {..} sol =
     case sol of
         Left _                     -> Nothing
-        Right (Solution m2sq ks _) -> do
-            let m2 = sqrt0 m2sq
-                Invisibles k1 k2 = mkInvisibles inp (vecToVars ks)
+        Right (Solution m2 ks _) -> do
+            let Invisibles k1 k2 = mkInvisibles inp (vecToVars ks)
             return $ M2Solution { _M2    = m2  * _scale
                                 , _k1sol = k1 ^* _scale
                                 , _k2sol = k2 ^* _scale }
-  where
-    sqrt0 x = if x < 0 then 1.0e+20 else sqrt x
 
 -- | (k1x, k1y, k1z, k2z).
 type Variables = (Double, Double, Double, Double)
@@ -121,56 +123,55 @@ mkInvisibles InputKinematics {..} (k1x, k1y, k1z, k2z) = Invisibles k1 k2
 -- | the unknowns are (k1x, k2x, k1z, k2z).
 m2ObjF :: InputKinematics -> Vector Double -> (Double, Vector Double)
 m2ObjF inp@InputKinematics {..} ks =
-    if m1sq < m2sq then (m2sq, grad2) else (m1sq, grad1)
+    if m1 < m2 then (m2, grad2) else (m1, grad1)
   where
-    (m1sq, m2sq, _) = m2ObjF' inp (vecToVars ks)
+    (m1, m2, _) = m2ObjF' inp (vecToVars ks)
     (grad1, grad2, _) = m2Grad inp _p1 _p2 ks
 
 m2ObjF' :: InputKinematics -> Variables -> (Double, Double, Invisibles)
-m2ObjF' inp@InputKinematics {..} ks = (m1sq, m2sq, invs)
+m2ObjF' inp@InputKinematics {..} ks = (m1, m2, invs)
   where
     invs@(Invisibles k1 k2) = mkInvisibles inp ks
-    m1sq = invariantMassSq [_p1, k1]
-    m2sq = invariantMassSq [_p2, k2]
+    m1 = invariantMass [_p1, k1]
+    m2 = invariantMass [_p2, k2]
 
 m2Grad :: InputKinematics
        -> FourMomentum  -- ^ p1 (or q1)
        -> FourMomentum  -- ^ p2 (or q2)
        -> Vector Double
        -> (Vector Double, Vector Double, Double)
-m2Grad inp@InputKinematics {..} p1 p2 ks = (d1, d2, deltaMsq)
+m2Grad inp@InputKinematics {..} p1 p2 ks = (d1, d2, m1 - m2)
   where
     vars@(k1x, k1y, k1z, k2z) = vecToVars ks
     (_, _, Invisibles k1 k2) = m2ObjF' inp vars
 
     (e1, p1x, p1y, p1z) = epxpypz p1
+    m1 = invariantMass [p1, k1]
     r1 = e1 / safeDivisor (energy k1)
-    d1 = fromList [ 2 * (r1 * k1x - p1x)
-                  , 2 * (r1 * k1y - p1y)
-                  , 2 * (r1 * k1z - p1z)
-                  , 0 ]
+    d1 = fromList $ ( / (2 * m1)) <$> [ 2 * (r1 * k1x - p1x)
+                                      , 2 * (r1 * k1y - p1y)
+                                      , 2 * (r1 * k1z - p1z)
+                                      , 0 ]
 
     (e2, p2x, p2y, p2z) = epxpypz p2
+    m2 = invariantMass [p2, k2]
     r2 = e2 / safeDivisor (energy k2)
-    d2 = fromList [ 2 * (r2 * (k1x - px _ptmiss) + p2x)
-                  , 2 * (r2 * (k1y - py _ptmiss) + p2y)
-                  , 0
-                  , 2 * (r2 * k2z - p2z) ]
-
-    deltaMsq = invariantMassSq [p1, k1] - invariantMassSq [p2, k2]
+    d2 = fromList $ ( / (2 * m2)) <$> [ 2 * (r2 * (k1x - px _ptmiss) + p2x)
+                                      , 2 * (r2 * (k1y - py _ptmiss) + p2y)
+                                      , 0
+                                      , 2 * (r2 * k2z - p2z) ]
 
 constraintF :: FourMomentum
             -> FourMomentum
             -> InputKinematics
             -> Vector Double
             -> (Double, Vector Double)
-constraintF p1 p2 inp ks = (deltaMsq, grad1 - grad2)
-  where (grad1, grad2, deltaMsq) = m2Grad inp p1 p2 ks
+constraintF p1 p2 inp ks = (deltaM, grad1 - grad2)
+  where (grad1, grad2, deltaM) = m2Grad inp p1 p2 ks
 
-constraintA :: InputKinematics -> Vector Double -> (Double, Vector Double)
+constraintA, constraintB
+    :: InputKinematics -> Vector Double -> (Double, Vector Double)
 constraintA inp@InputKinematics {..} = constraintF _p1 _p2 inp
-
-constraintB :: InputKinematics -> Vector Double -> (Double, Vector Double)
 constraintB inp@InputKinematics {..} = constraintF _q1 _q2 inp
 
 -- | unsafe transformation, but it would be fine.
